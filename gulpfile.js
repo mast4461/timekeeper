cf9c9d13e78bce58474ebfb578f812eaa1863fad
Martin Stålberg <mast4461@gmail.com>
2016-02-02 22:24:26 +0100

    Minor changes. - gulpfile.js: Better formatting. Also removed unused server options for browsersync. - index.html: Changed formatting.

--- gulpfile.js
+++ gulpfile.js
@@ -1,49 +1,45 @@
-var gulp 			= require('gulp'),
-	browserify 		= require('gulp-browserify'),
-	sass  			= require('gulp-ruby-sass'),
+var gulp			= require('gulp'),
+	browserify		= require('gulp-browserify'),
+	sass			= require('gulp-ruby-sass'),
 	browserSync 	= require('browser-sync'),
-	reload 			= browserSync.reload;
+	reload			= browserSync.reload;
 
 
 //Tasks  regarding scripts--------------------------------------------------|
 gulp.task('scripts', function(){
-// Single entry point to browserify
-       gulp.src('scripts/main.js')
-       	.pipe(browserify({
-	       	 insertGlobals : true,
-	        	debug : !gulp.env.production
-        	}))
-       	.pipe(gulp.dest('dist'))
+	// Single entry point to browserify
+	gulp.src('scripts/main.js')
+		.pipe(browserify({
+			insertGlobals : true,
+			debug : !gulp.env.production
+		}))
+		.pipe(gulp.dest('dist'));
 });
 
 //Tasks regarding styles----------------------------------------------------|
 gulp.task('sass', function(){
 	return sass('styles/main.sass')
-	.pipe(gulp.dest('dist'))
+		.pipe(gulp.dest('dist'))
 });
 
 //Live reload--------------------------------------------------------------------|
 var liveReload = function () {
-
 	// Compile if sass or js has changed
 	gulp.watch('styles/**/*.sass', ['sass']);
 	gulp.watch('scripts/**/*.js', ['scripts']);
 
 	// Reload if new compiled files have been generated or index.html updates
 	gulp.watch([
-		'dist/**/*',
-		'index.html'
-	]).on('change', reload);
+			'dist/**/*',
+			'index.html'
+		]).on('change', reload);
 
 	browserSync({
 		notify: false,
 		port: 3600,
-		server: {
-			routes: {
-				'/bower_components': 'bower_components'
-			}
-		}
+		server: {}
 	});
 };
+
 gulp.task('serve', ['scripts','sass'], liveReload);
 gulp.task('default', ['scripts','sass'], liveReload);
